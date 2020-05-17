9948dfb3aa919896ccf21cceaa209db2e61217ae
龙张 <callme-z@qq.com>
2020-05-18 00:01:26 +0800

    移除ListConfig用法  想复杂了

--- src/Http/Controllers/Frontend/Api/SettingsController.php
+++ src/Http/Controllers/Frontend/Api/SettingsController.php
@@ -4,10 +4,7 @@
 namespace Modules\Core\Http\Controllers\Frontend\Api;
 
 
-use Illuminate\Http\Request;
-use Modules\Core\Config\Repository;
 use Modules\Core\Http\Controllers\Controller;
-use Modules\Core\Services\Frontend\ConfigService;
 
 class SettingsController extends Controller
 {
