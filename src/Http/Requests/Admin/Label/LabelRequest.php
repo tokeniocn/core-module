98392acb1b43b231ac21130da0e7181b18ccab63
aoxiang <makenmail@qq.com>
2020-05-19 20:16:45 +0800

    markdown 的包，相关的js \css

---
+++ src/Http/Requests/Admin/Label/LabelRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modules\Core\Http\Requests\Admin\Label;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LabelRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->isAdmin();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'key' => "required",
+            "value" => "required",
+            "remark" => 'required'
+        ];
+    }
+}
