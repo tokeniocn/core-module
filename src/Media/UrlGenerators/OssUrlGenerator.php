03218df1cb2c2b6ffa4b74a24e904c8fabb21320
龙张 <callme-z@qq.com>
2020-05-28 18:49:40 +0800

    增加OSS URL generator

---
+++ src/Media/UrlGenerators/OssUrlGenerator.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Modules\Core\Media\UrlGenerators;
+
+use Plank\Mediable\UrlGenerators\BaseUrlGenerator;
+use Illuminate\Contracts\Config\Repository as Config;
+use Illuminate\Contracts\Filesystem\Cloud;
+use Illuminate\Filesystem\FilesystemManager;
+
+class OssUrlGenerator extends BaseUrlGenerator
+{
+    /**
+     * Filesystem Manager.
+     * @var FilesystemManager
+     */
+    protected $filesystem;
+
+    /**
+     * Constructor.
+     * @param \Illuminate\Contracts\Config\Repository $config
+     * @param \Illuminate\Filesystem\FilesystemManager $filesystem
+     */
+    public function __construct(Config $config, FilesystemManager $filesystem)
+    {
+        parent::__construct($config);
+        $this->filesystem = $filesystem;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAbsolutePath(): string
+    {
+        return $this->getUrl();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUrl(): string
+    {
+        /** @var Cloud $filesystem */
+        $filesystem = $this->filesystem->disk($this->media->disk);
+        return $filesystem->url($this->media->getDiskPath());
+    }
+}
