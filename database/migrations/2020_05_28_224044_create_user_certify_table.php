1a961a7527e11d491baed31aa0f9eab81501bf06
龙张 <callme-z@qq.com>
2020-05-29 14:47:43 +0800

    no message

--- database/migrations/2020_05_28_224044_create_user_certify_table.php
+++ database/migrations/2020_05_28_224044_create_user_certify_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Modules\Core\Models\Frontend\UserCertify;
 
 class CreateUserCertifyTable extends Migration
 {
@@ -19,16 +20,13 @@ class CreateUserCertifyTable extends Migration
             $table->string('name')->default('')->comment('名字');
             $table->string('obverse')->default('')->comment('正面照');
             $table->string('reverse')->default('')->comment('反面照');
-            $table->unsignedTinyInteger('certify_type')->default(\Modules\Core\Models\Frontend\UserCertify::CERTIFY_TYPE_IDENTIFICATION)->comment('实名认证证件类型');
+            $table->unsignedTinyInteger('certify_type')->default(UserCertify::CERTIFY_TYPE_IDENTIFICATION)->comment('实名认证证件类型');
             $table->string('number')->default('')->comment('证件号码');
-            $table->unsignedTinyInteger('status')->default(\Modules\Core\Models\Frontend\UserCertify::STATUS_WAITING)->comment('0=未审核 1=审核通过 -1=驳回');
+            $table->unsignedTinyInteger('status')->default(UserCertify::STATUS_WAITING)->comment('0=未审核 1=审核通过 -1=驳回');
             $table->dateTime('created_at')->nullable();
             $table->dateTime('updated_at')->nullable();
             $table->dateTime('deleted_at')->nullable();
         });
-
-        DB::statement("ALTER TABLE `" . config('database.connections.mysql.prefix') . "user_certifies` comment'用户实名认证'"); // 表注释
-
     }
 
     /**
