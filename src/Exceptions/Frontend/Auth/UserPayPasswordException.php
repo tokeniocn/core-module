c667985d47b1288d6fc914d6a700b44b65171554
aoxiang <makenmail@qq.com>
2020-06-03 20:30:57 +0800

    增加支付密码错误Exception

---
+++ src/Exceptions/Frontend/Auth/UserPayPasswordException.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Modules\Core\Exceptions\Frontend\Auth;
+
+use Exception;
+
+/**
+ * 支付密码异常
+ * Class UserPayPasswordException
+ * @package Modules\Core\Exceptions\Frontend\Auth
+ */
+class UserPayPasswordException extends Exception
+{
+
+}
