7196acf629b493596ca09d80a59e7cfd723ea57e
龙张 <callme-z@qq.com>
2020-06-05 21:38:41 +0800

    no message

--- Database/Migrations/2020_06_04_152150_create_user_banks_table.php
+++ Database/Migrations/2020_06_04_152150_create_user_banks_table.php
@@ -19,8 +19,8 @@ class CreateUserBanksTable extends Migration
             $table->string('bank')->default('')->comment('银行类型 wechat-微信 alipay-支付宝 bank-银行卡信息');
             $table->text('value')->comment('存储值,包括解析出来的二维码内容');
             $table->boolean('enable')->default(false)->comment('是否启用');
-            $table->dateTimeTz('created_at');
-            $table->dateTimeTz('updated_at');
+            $table->dateTimeTz('created_at')->nullable();
+            $table->dateTimeTz('updated_at')->nullable();
             $table->dateTimeTz('deleted_at')->nullable();
         });
     }
