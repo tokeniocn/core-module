000a296b67d0477d8452fb6a8a85bc2bb7aecb28
小墨 <1123955716@qq.com>
2020-06-23 18:21:50 +0800

    实现用短信和邮箱发送非验证码功能（发送系统钱包预警提示发送信息）

---
+++ src/Messages/SystemWalletBalanceEmailMessage.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Modules\Core\Messages;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class SystemWalletBalanceEmailMessage extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public $email;
+    public $data; //主要放置一些消息提示参数
+
+    public function __construct($email, array $data = [])
+    {
+        $this->email = $email;
+        $this->data = $data;
+    }
+
+
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     *
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+
+        $notifiable->route('mail', $this->email);
+
+        $limeMsg = env('APP_NAME') . '系统钱包余额预警。';
+        if (isset($this->data['line_msg'])) {
+            $limeMsg .= "详情：" . $this->data['line_msg'];
+        }
+        return (new MailMessage())
+            ->subject($this->data['subject_msg'] ?? '系统钱包余额邮件通知')
+            ->greeting($this->data['greeting_msg'] ?? '系统钱包管理者您好：')
+            ->line($limeMsg);
+
+    }
+
+
+}
--- src/Services/Frontend/NotificationService.php
+++ src/Services/Frontend/NotificationService.php
@@ -3,7 +3,17 @@
 namespace Modules\Core\Services\Frontend;
 
 use Illuminate\Notifications\AnonymousNotifiable;
-use Modules\Core\Notifications\Frontend\SendEmailMsg;
+
+/*use Modules\Core\Notifications\Frontend\SystemWalletBalanceWarning;*/
+
+use Illuminate\Notifications\Messages\MailMessage;
+
+//use Modules\Core\Messages\EmailMessage;
+use Illuminate\Notifications\Notification;
+use Leonis\Notifications\EasySms\Messages\EasySmsMessage as MobileMessage;
+use Modules\Core\Messages\SystemWalletBalanceEmailMessage;
+use Modules\Core\Notifications\Frontend\SendMobileMsg;
+use Modules\Core\Notifications\Frontend\email\SystemWalletBalanceWarning;
 use Modules\Core\Notifications\Frontend\UserEmailVerify;
 use Modules\Core\Services\Traits\HasThrottles;
 use Modules\Core\Notifications\Frontend\UserMobileVerify;
@@ -40,7 +50,7 @@ class NotificationService
 
             $user->sendMobileVerifyNotification($verify);
         } else {
-            $verify = $userVerifyService->createWithKey($mobile, $type, $token, null,$options['createOptions'] ?? []);
+            $verify = $userVerifyService->createWithKey($mobile, $type, $token, null, $options['createOptions'] ?? []);
 
             /** @var AnonymousNotifiable $notifiable */
             $notifiable = resolve(AnonymousNotifiable::class);
@@ -75,7 +85,7 @@ class NotificationService
 
             $user->sendEmailVerifyNotification($verify);
         } else {
-            $verify = $userVerifyService->createWithKey($email, $type, null, null,$options['createOptions'] ?? []);
+            $verify = $userVerifyService->createWithKey($email, $type, null, null, $options['createOptions'] ?? []);
 
             /** @var AnonymousNotifiable $notifiable */
             $notifiable = resolve(AnonymousNotifiable::class);
@@ -88,49 +98,63 @@ class NotificationService
 
 
     /**
-     * 发送非验证码通知邮件
-     * @param $email
-     * @param $type
-     * @param null $user
+     * 发送非验证码短信
+     * 具体实现类需继承Notification类
+     * 没有相关的专项服务类，请自行后续扩展
+     * @param $mobile
+     * @param MobileMessage $message
      * @param array $options
      * @return bool
-     * @throws \Illuminate\Validation\ValidationException
      */
-    public function sendEmailMsgNotification($email, $type, array $data, array $options = [])
+    public function sendMobileMsgNotification($mobile, Notification $message, array $options = [])
     {
-        /*$key = $email . '|' . $type;
-        $this->checkKeyAttempts(
-            $key,
-            config('core::system.notification_email_maxAttempts', 3),
-            config('core::system.notification_email_decaySeconds', 600)
-        );*/
+        $limitTimes = $options['limit_times'] ?? false;
+        if ($limitTimes) { //是否限制发送频率
+            $key = $mobile . '|' . get_class($message);
+            $this->checkKeyAttempts(
+                $key,
+                config('core::system.notification_mobile_maxAttempts', 3),
+                config('core::system.notification_mobile_decaySeconds', 600)
+            );
+        }
 
-        //$userVerifyService = resolve(UserVerifyService::class);
-
-//        if ($user) {
-//            $user = with_user($user);
-//            $verify = $userVerifyService->createWithUser($user, $email, $type, null, null, $options['createOptions'] ?? []);
-//
-//            $user->sendEmailVerifyNotification($verify);
-//        } else {
-//            $verify = $userVerifyService->createWithKey($email, $type, null, null,$options['createOptions'] ?? []);
-//
-//            /** @var AnonymousNotifiable $notifiable */
-//            $notifiable = resolve(AnonymousNotifiable::class);
-//            $notifiable->notify(new UserEmailVerify($verify));
-//        }
-
-
-        $data['key'] = '1123955716@qq.com';//$email;
-        $data['subject'] = "邮件通知";
-        $data['line'] = "币价不正常！！！";
         /** @var AnonymousNotifiable $notifiable */
         $notifiable = resolve(AnonymousNotifiable::class);
-        $notifiable->notify(new SendEmailMsg($data));
+        $notifiable->notify($message);
 
         return true;
     }
 
+
+    /**
+     * 发送非验证码通知邮件
+     * 具体实现类需继承Notification类
+     * 没有相关的专项服务类，请自行后续扩展
+     * @param $email
+     * @param Notification $message
+     * @param array $options
+     * @return bool
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function sendEmailMsgNotification($email, Notification $message, array $options = [])
+    {
+
+        $limitTimes = $options['limit_times'] ?? false;
+        if ($limitTimes) { //是否限制发送频率
+            $key = $email . '|' . get_class($message);
+            $this->checkKeyAttempts(
+                $key,
+                config('core::system.notification_email_maxAttempts', 3),
+                config('core::system.notification_email_decaySeconds', 600)
+            );
+        }
+
+
+        /** @var AnonymousNotifiable $notifiable */
+        $notifiable = resolve(AnonymousNotifiable::class);
+        $notifiable->notify($message); //邮件一般由队列发送
+        return true;
+    }
 
 
 }
