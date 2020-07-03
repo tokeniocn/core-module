d185194a6ffe70f86354208021a7960ffe977e5f
敖翔 <makenmail@qq.com>
2020-07-03 15:41:51 +0800

    处理default_coin_list变成对象的问题。

--- src/Http/Controllers/Admin/App/SettingsController.php
+++ src/Http/Controllers/Admin/App/SettingsController.php
@@ -13,76 +13,76 @@ class SettingsController extends Controller
 {
 
     protected $schema = [
-        'app_logo' => [
-            'key' => 'app_logo',
-            'type' => 'image',
-            'title' => 'Logo',
-            'value' => '',
+        'app_logo'          => [
+            'key'         => 'app_logo',
+            'type'        => 'image',
+            'title'       => 'Logo',
+            'value'       => '',
             'description' => 'App Logo',
         ],
-        'app_name' => [
-            'key' => 'app_name',
-            'type' => 'text',
-            'title' => '系统名称',
-            'value' => 'App名字',
+        'app_name'          => [
+            'key'         => 'app_name',
+            'type'        => 'text',
+            'title'       => '系统名称',
+            'value'       => 'App名字',
             'description' => 'App 名字',
         ],
-        'app_cover_images' => [
-            'key' => 'cover_images',
-            'type' => 'image_list',
-            'title' => '启动图',
-            'value' => [],
+        'app_cover_images'  => [
+            'key'         => 'cover_images',
+            'type'        => 'image_list',
+            'title'       => '启动图',
+            'value'       => [],
             'description' => 'App启动图',
         ],
-        'share_link' => [
-            'key' => 'share_link',
-            'type' => 'text',
-            'title' => '分享链接',
-            'value' => '',
+        'share_link'        => [
+            'key'         => 'share_link',
+            'type'        => 'text',
+            'title'       => '分享链接',
+            'value'       => '',
             'description' => '分享链接',
         ],
-        'default_coin' => [
-            'key' => 'default_coin',
-            'type' => 'radio',
-            'title' => '默认币种',
-            'value' => 'USDT',
-            'data' => [],
+        'default_coin'      => [
+            'key'         => 'default_coin',
+            'type'        => 'radio',
+            'title'       => '默认币种',
+            'value'       => 'USDT',
+            'data'        => [],
             'description' => '默认币种',
         ],
         'default_coin_list' => [
-            'key' => 'default_coin_list',
-            'type' => 'checkbox',
-            'title' => '可用币种列表',
-            'value' => ['USDT', 'ETH', 'BTC'],
-            'data' => [],
+            'key'         => 'default_coin_list',
+            'type'        => 'checkbox',
+            'title'       => '可用币种列表',
+            'value'       => ['USDT', 'ETH', 'BTC'],
+            'data'        => [],
             'description' => '全部可以用的币种',
         ],
-        'ios_version' => [
-            'key' => 'ios_version',
-            'type' => 'text',
-            'title' => '苹果版本号',
-            'value' => '1.0.0',
+        'ios_version'       => [
+            'key'         => 'ios_version',
+            'type'        => 'text',
+            'title'       => '苹果版本号',
+            'value'       => '1.0.0',
             'description' => 'IOS Version ',
         ],
-        'ios_download' => [
-            'key' => 'ios_download',
-            'type' => 'text',
-            'title' => '苹果下载地址',
-            'value' => '1.0.0',
+        'ios_download'      => [
+            'key'         => 'ios_download',
+            'type'        => 'text',
+            'title'       => '苹果下载地址',
+            'value'       => '1.0.0',
             'description' => '正式版下载地址',
         ],
-        'android_version' => [
-            'key' => 'android_version',
-            'type' => 'text',
-            'title' => '安卓版本号',
-            'value' => '1.0.0',
+        'android_version'   => [
+            'key'         => 'android_version',
+            'type'        => 'text',
+            'title'       => '安卓版本号',
+            'value'       => '1.0.0',
             'description' => 'android Version ',
         ],
-        'android_download' => [
-            'key' => 'android_download',
-            'type' => 'text',
-            'title' => '安卓下载地址',
-            'value' => '1.0.0',
+        'android_download'  => [
+            'key'         => 'android_download',
+            'type'        => 'text',
+            'title'       => '安卓下载地址',
+            'value'       => '1.0.0',
             'description' => '正式版下载地址',
         ],
         /*'share_link' => [
@@ -92,18 +92,18 @@ class SettingsController extends Controller
             'value' => '',
             'description' => '分享链接',
         ],*/
-        'admin_mobile' => [
-            'key' => 'admin_mobile',
-            'type' => 'text',
-            'title' => '管理员手机',
-            'value' => '',
+        'admin_mobile'      => [
+            'key'         => 'admin_mobile',
+            'type'        => 'text',
+            'title'       => '管理员手机',
+            'value'       => '',
             'description' => '可用于接收系统通知',
         ],
-        'admin_email' => [
-            'key' => 'admin_email',
-            'type' => 'text',
-            'title' => '管理员邮箱',
-            'value' => '',
+        'admin_email'       => [
+            'key'         => 'admin_email',
+            'type'        => 'text',
+            'title'       => '管理员邮箱',
+            'value'       => '',
             'description' => '可用于接收系统通知',
         ],
     ];
@@ -121,10 +121,11 @@ class SettingsController extends Controller
     protected function normalizeSchema(array $data)
     {
         /** @var CoinService $coinService */
-        $coinService = resolve(CoinService::class);
-        $coins = $coinService->all();
-        $coinNames = $coins->pluck('symbol','symbol');
+        $coinService                               = resolve(CoinService::class);
+        $coins                                     = $coinService->all();
+        $coinNames                                 = $coins->pluck('symbol', 'symbol');
         $this->schema['default_coin_list']['data'] = $this->schema['default_coin']['data'] = $coinNames;
+
         return array_map(function ($value) use ($data) {
             return array_merge($value, [
                 'value' => $data[$value['key']] ?? $value['value'],
@@ -134,13 +135,18 @@ class SettingsController extends Controller
     }
 
     /**
-     * @param ConfigRequest $request
+     * @param  ConfigRequest  $request
      *
      * @return \Illuminate\Http\RedirectResponse
      */
     public function update(ConfigRequest $request)
     {
-        store_config('api::settings', $request->post());
+        $data = $request->post();
+
+        if (isset($data['default_coin_list'])) {
+            $data['default_coin_list'] = array_values($data['default_coin_list']);
+        }
+        store_config('api::settings', $data);
 
         return response()->redirectTo(route('admin.app.settings.index'));
     }
